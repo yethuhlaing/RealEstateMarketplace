dd8c2ed0fa25c14aa0b5cc17479ce5ae13f2dcd6
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2023-12-13 23:24:09 +0200

    Create the Model, Route, Controller structure

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const test = (req,res) => {
+    res.json({
+        message: "API route is working!"
+    })
+}
+export default test;
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js"
 dotenv.config()
 mongoose.connect(process.env.MONGO).then( ()=> {
         console.log("Connected to Mongo")
@@ -8,6 +9,9 @@ mongoose.connect(process.env.MONGO).then( ()=> {
         console.log(err)
     })
 const app = express();
+app.use(userRouter)
+
+
 
 app.listen(3000, ()=> {
     console.log("Server is ruunning on port 3000!")
