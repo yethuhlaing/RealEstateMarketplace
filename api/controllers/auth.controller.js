4cc88693087f4ad3c6fcf2658e3f91c2110c2e96
yethuhlaing <Ye.Thu.Hlaing@student.lab.fi>
2023-12-14 23:23:59 +0200

    Create a middleware and a custom function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
+
 const signup = async (req, res) => {
     const { username , email, password } = req.body;
     const hashPassword = bcryptjs.hashSync(password, 10);
@@ -9,7 +10,7 @@ const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json("User Created Successfully!");
     } catch (error) {
-        res.status(500).json(error.message)
+        next()
     }
     console.log(newUser)
 
--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
-
 dotenv.config()
 const PORT = parseInt(process.env.PORT, 10);
 const HOST = process.env.HOST;
@@ -19,7 +18,15 @@ const app = express();
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
+app.use( (err, req, res, next)=> {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json( {
+        success: false,
+        statusCode,
+        message
+    })
+})
 
 
 app.listen(PORT, HOST, ()=> {
